005c5c7ffef382be11b01a16d20febf1cb4ac17a
Maneesha-Maduranga <maneeshamaduranga22@gmail.com>
2023-11-28 21:30:45 +0530

    Add database connection function

---
+++ database/connectDb.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Connected To The Database");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  connectDB,
+};
